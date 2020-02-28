df428978a448c384de18f4795b3ca659f507d929
jayakrishnanu <jkrishnanu@gmail.com>
2020-02-27 20:54:35 -0800

    Add the cocoa framework

---
+++ TestCocoa/TestCocoa/TestCocoa.h
@@ -0,0 +1,19 @@
+//
+//  TestCocoa.h
+//  TestCocoa
+//
+//  Created by Jayakrishnan u on 2/27/20.
+//  Copyright © 2020 Jayakrishnan u. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCocoa.
+FOUNDATION_EXPORT double TestCocoaVersionNumber;
+
+//! Project version string for TestCocoa.
+FOUNDATION_EXPORT const unsigned char TestCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoa/PublicHeader.h>
+
+
---
+++ TestCocoa/TestCocoa/TestCocoa.swift
@@ -0,0 +1,18 @@
+//
+//  TestCocoa.swift
+//  TestCocoa
+//
+//  Created by Jayakrishnan u on 2/27/20.
+//  Copyright © 2020 Jayakrishnan u. All rights reserved.
+//
+
+import Foundation
+
+class TestCocoa {
+    
+    private init() { }
+    
+    public static func simpleFunction() -> String {
+        return "Hello this is Jayakrishnan"
+    }
+}
